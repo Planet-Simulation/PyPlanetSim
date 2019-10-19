b4e39355c6857bb6da4c51067f607da3c65b9590
AlfonsoMVs <alfmatvic@gmail.com>
2019-10-19 15:45:05 +0200

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,27 +1,26 @@
 from math import cos, radians
 
 generate_matrix = lambda y,x,a: [[a for x in range(x)] for i in range(y)]
+ec_temp_effective = lambda K,a: ((K*(1-a))/(4*5.6705*10**(-8)))**(1/4) # Temperatura effective's ecuation
 
 def slant(n,m):
 	inclination = []
 	angle = 180/(n-1)
-	inclination.append(generate_matrix(1,m,0)[0])
-	for apex in range(1,int((n-1)/2)): #for each different latitude, the same angle is added to de matrix in the 0-position and in the last position.
+	inclination.append(generate_matrix(1,m,0))
+	for apex in range(1,int((n-1)/2)): # For each different latitude, the same angle is added to de matrix in the 0-position and in the last position.
 		c_angle = angle*apex
 		row_n = []
 		for i in range(0,m): row_n.append(c_angle)
 		inclination.append(row_n)
 		inclination.insert(0,row_n)
-
 	return inclination
 
-
-def heat(inclination,temperature_field,sun_h):
-	new_temperature_field = generate_matrix(len(inclination),len(inclination[0]))
+def heat(inclination,temperature_field,colours,K):
+	new_temperature_field = generate_matrix(len(inclination),len(inclination[0]), 0)
 	for i in range(0,len(inclination)):
 		for k in range(0,len(inclination[0])):
-			sun_heat = cos(radians(inclination[i][k]))*sun_h
-			new_temperature_field[i][k] = (sun_heat+temperature_field[i][k])/2
-
+			sun_heat = (cos(radians(inclination[i][k]))*ec_temp_effective(K,colours[i][k]))
+			neigh_warm = (temperature_field[i+1][k]+temperature_field[i-1][k]+temperature_field[i][k+1]+temperature_field[i][k-1])/4
+			new_temperature_field[i][k] = 0.4*(sun_heat+temperature_field[i][k])+neigh_warm*0.2
 	return new_temperature_field
 
--- main.py
+++ main.py
@@ -1,23 +1,38 @@
-from functions import generate_matrix, slant, heat
+from functions import generate_matrix, slant, heat, ec_temp_effective
 from math import cos, radians
 from random import random
 
+# Main Parameters
+n_cols = 10 # Columns' number (y)
+n_rows = 10 # Rows number (x)
+prop_popu = 0.3 # Proportion of initial population
+prop_blacks = 0.3 # Proportion of Blacks
+solar_const = 1361 # Solar constant (K)
+planet_alb = 0.3 # Percentage of planetary albedo
 
-def starting_conditions(n,m,population_chance,blacks):
+def starting_conditions(n,m,population_chance,blacks,sol_const,perc_alb):
 	population = generate_matrix(n,m,0)
 	colours = generate_matrix(n,m,0)
+	temp_field =  generate_matrix(n,m,0)
 	for i in range(0,n):
 		for k in range(0,m):
 			if random()<population_chance:
 				population[i][k] = 1
-				if random()<blacks: colours[i][k] = 1
-				else: colours[i][k] = 0
+				if random()<blacks: 
+					colours[i][k] = 0.1
+					temp_field[i][k] = cos(radians(slant(n,m)[i][k]))*ec_temp_effective(solar_const,0.1)
+				else: 
+					colours[i][k] = 0.9
+					temp_field[i][k] = cos(radians(slant(n,m)[i][k]))*ec_temp_effective(solar_const,0.9)
 			else:
 				population[i][k] = 0
-				colours[i][k] = -1
-	return [population,colours]
+				colours[i][k] = perc_alb
+				temp_field[i][k] = cos(radians(slant(n,m)[i][k]))*ec_temp_effective(solar_const,perc_alb)
+	return [population,colours,temp_field]
 
 
+print(starting_conditions(n_cols,n_rows,prop_popu,prop_blacks,solar_const,planet_alb))
 
-#while (final_conditions==False):
-
+# Salta error ya que durante las starting conditions he puestos "slant(n,m)[i][k]" pero resulta que en la función
+# slant no me crea una matriz de dichas dimensiones, por lo tanto, simplemente arregla el slant, no toques nada
+# de este archivo y préndelo.
